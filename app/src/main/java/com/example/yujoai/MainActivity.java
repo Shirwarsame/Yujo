ca8b10062d173b9d81d96319d3f19451ecb19837
GD <gindulink@gmail.com>
2020-04-02 23:07:20 -0400

    Add speech to text functionality

--- app/src/main/java/com/example/yujoai/MainActivity.java
+++ app/src/main/java/com/example/yujoai/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.yujoai;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    // Will contain output speech of User
+    private TextView tvxResult;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvxResult = (TextView) findViewById(R.id.txvResult);
+
+    }
+
+    //Gets what User said
+    public void getSpeechInput(View view)
+    {
+        // Request user to speak and pass through a Recognizer
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+
+        // Takes user input as free form
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+        if(intent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivityForResult(intent, 10);
+        }
+        else
+        {
+            Toast.makeText(this, "Speech is not supported", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode)
+        {
+            case 10:
+                if(resultCode == RESULT_OK && data != null)
+                {
+                   ArrayList<String> results =  data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                }
+        }
     }
 }
