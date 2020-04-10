af69f86c71884c218f094dc0863762ee22135fb2
GD <gindulink@gmail.com>
2020-04-10 00:22:49 -0400

    Verify SD availability

--- app/src/main/java/com/example/yujoai/MainActivity.java
+++ app/src/main/java/com/example/yujoai/MainActivity.java
@@ -1,41 +1,63 @@
 package com.example.yujoai;
 
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import java.util.Locale;
-import android.speech.tts.TextToSpeech;
-import android.speech.RecognizerIntent;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-
+import org.alicebot.ab.AIMLProcessor;
 import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+import org.alicebot.ab.Graphmaster;
+import org.alicebot.ab.MagicBooleans;
+import org.alicebot.ab.MagicStrings;
+import org.alicebot.ab.PCAIMLProcessorExtension;
+import org.alicebot.ab.Timer;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Locale;
+//Main AMIC https://github.com/Hariofspades/ChatBot
 
 //Prebuit AIMC Engine used from https://github.com/Hariofspades/ChatBot
 
 public class MainActivity extends AppCompatActivity {
 
-    // Will contain output speech of User
-    private TextView tvxResult;
-    //Contains text to convert
-    private TextToSpeech textToSpeech;
-    private Boolean stillTalking = true;
-
+    private Button mButtonSend;
+    private EditText mEditTextMessage;
+    private ImageView mImageView;
     public Bot bot;
+    public static Chat chat;
+    private ChatMessageAdapter mAdapter;
+    private TextToSpeech textToSpeech;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvxResult = (TextView) findViewById(R.id.txvResult);
+        mButtonSend = (Button) findViewById(R.id.btn_send);
+        mEditTextMessage = (EditText) findViewById(R.id.et_message);
+        mImageView = (ImageView) findViewById(R.id.iv_image);
+        mAdapter = new ChatMessageAdapter(this, new ArrayList<ChatMessage>());
 
-        // Initialize obj to contain text we want Yujo to talk back
+
         textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             // Verification if feature is supported
@@ -55,8 +77,20 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
     }
+        //check SD card availability
+        public static boolean isSDCARDAvailable(){
+            return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)? true :false;
+        }
+        //copying the file
+        private void copyFile(InputStream in, OutputStream out) throws IOException {
+            byte[] buffer = new byte[1024];
+            int read;
+            while((read = in.read(buffer)) != -1){
+                out.write(buffer, 0, read);
+            }
+        }
+
 
     //Gets what User said
     public void getSpeechInput(View view) {
