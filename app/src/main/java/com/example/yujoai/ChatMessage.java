ac3894fa2627489bdbab10238a4d09bb9326302e
GD <gindulink@gmail.com>
2020-04-09 23:18:30 -0400

    Add ChatMessage class

---
+++ app/src/main/java/com/example/yujoai/ChatMessage.java
@@ -0,0 +1,25 @@
+package com.example.yujoai;
+
+public class ChatMessage {
+    private boolean isImage, isMine;
+    private String content;
+
+    public ChatMessage(String message, boolean mine, boolean image) {
+        content = message;
+        isMine = mine;
+        isImage = image;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public boolean isMine() {
+        return isMine;
+    }
+
+    public boolean isImage() {
+        return isImage;
+    }
+
+}
