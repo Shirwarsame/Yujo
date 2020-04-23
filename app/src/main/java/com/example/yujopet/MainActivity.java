b1bf3898d411fa029a26ba8320eae699e56c3fb1
Shirwa Warsame <shirwarsame@gmail.com>
2020-04-22 23:13:09 -0400

    Added AI Speech Functionality

--- app/src/main/java/com/example/yujopet/MainActivity.java
+++ app/src/main/java/com/example/yujopet/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.yujopet;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -9,9 +11,15 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.Voice;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
@@ -31,17 +39,42 @@ import com.google.ar.sceneform.ux.TransformableNode;
 
 import android.os.Bundle;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Locale;
+
+
+import org.alicebot.ab.AIMLProcessor;
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+import org.alicebot.ab.Graphmaster;
+import org.alicebot.ab.MagicBooleans;
+import org.alicebot.ab.MagicStrings;
+import org.alicebot.ab.PCAIMLProcessorExtension;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final double MIN_OPENGL_VERSION = 3.0;
 
     int mBackgroundIndex = 0; //Buddha is default
 
-    Button mShowARButton,mUpButton, mDownButton,mRestartButton;
+    //Adding speech capabilities
+    public Bot bot;
+    public static Chat chat;
+    private TextToSpeech tts;
+    private Voice voice;
+
+
+    Button mShowARButton,mUpButton, mDownButton,mRestartButton, mButtonSend;
     ArFragment arFragment;
 
     ModelRenderable virtualPetRenderable,virtualPetRenderable2,virtualPetRenderable3;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+        mButtonSend = (Button) findViewById(R.id.talkButton);
         mShowARButton = (Button) findViewById(R.id.buttonDisplay);
         mDownButton = (Button) findViewById(R.id.buttonDown);
         mUpButton = (Button) findViewById(R.id.buttonUp);
@@ -162,7 +196,64 @@ public class MainActivity extends AppCompatActivity {
                 });
 
 
+        // Initialize obj for Speech Output
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            // Verification if feature is supported
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    voice = new Voice("en-us-x-sfg#male_1-local",
+                            Locale.getDefault(), 1, 1, false, null);
+                    tts.setVoice(voice);
+                }
+            }
 
+        });
+
+        //Checking SD card availablility
+        boolean a = isSDCARDAvailable();
+        //Receiving the assets from the app directory
+        AssetManager assets = getResources().getAssets();
+        File jayDir = new File(Environment.getExternalStorageDirectory().toString() + "/hari/bots/Hari");
+        boolean b = jayDir.mkdirs();
+        if (jayDir.exists()) {
+            //Reading the file
+            try {
+                for (String dir : assets.list("Hari")) {
+                    File subdir = new File(jayDir.getPath() + "/" + dir);
+                    boolean subdir_check = subdir.mkdirs();
+                    for (String file : assets.list("Hari/" + dir)) {
+                        File f = new File(jayDir.getPath() + "/" + dir + "/" + file);
+                        if (f.exists()) {
+                            continue;
+                        }
+                        InputStream in = null;
+                        OutputStream out = null;
+                        in = assets.open("Hari/" + dir + "/" + file);
+                        out = new FileOutputStream(jayDir.getPath() + "/" + dir + "/" + file);
+                        //Copy file from assets to the mobile's SD card or any secondary memory
+                        copyFile(in, out);
+                        in.close();
+                        in = null;
+                        out.flush();
+                        out.close();
+                        out = null;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //Get the working directory where assets are stored
+        MagicStrings.root_path = Environment.getExternalStorageDirectory().toString() + "/hari";
+        System.out.println("Working Directory = " + MagicStrings.root_path);
+        AIMLProcessor.extension =  new PCAIMLProcessorExtension();
+        //Initialize user request and bot response
+        bot = new Bot("hari", MagicStrings.root_path, "chat");
+        chat = new Chat(bot);
+        String[] args = null;
+        mainFunction(args);
 
     }
 
@@ -280,7 +371,75 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    //Check SD card availability
+    public static boolean isSDCARDAvailable(){
+        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)? true :false;
+    }
+    //Copying the file
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
+    }
 
+    //Gets what User said
+    public void getSpeechInput(View view) {
+        // Request user to speak and pass through a Recognizer
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+        // Takes user input as free form
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+        // Verify if taking input speech is supported
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 10);
+        } else {
+            Toast.makeText(this, "Speech is not supported", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    //Executed after input speech is recorded
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 10:
+                // Extracting data from Recognizer and adding into Results
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    // After data is extracted move to AI processing
+                    processData(results);
+                }
+        }
+    }
+    // Handles Speech Output to User
+    void outputResponse(String response) {
+        int speechStatus = tts.speak(response, TextToSpeech.QUEUE_FLUSH, null);
+
+        if (speechStatus == TextToSpeech.ERROR) {
+            Log.e("TTS", "Error in converting Text to Speech!");
+        }
+    }
+
+    // Processes input data and obtain response from AI
+    public void processData(ArrayList<String> processingData) {
+        String message = processingData.get(0);
+        //bot
+        String response = chat.multisentenceRespond(message);
+        if (TextUtils.isEmpty(message)) {
+            return;
+        }
+        outputResponse(response);
+    }
+
+    public static void mainFunction(String[] args) {
+        MagicBooleans.trace_mode = false;
+        System.out.println("trace mode = " + MagicBooleans.trace_mode);
+        Graphmaster.enableShortCuts = true;
+    }
 }
 
 
